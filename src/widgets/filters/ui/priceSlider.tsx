18e57d348e90bbdf8a12b3175f3509fdb9a55035
Yaroslava <yaroslava.ger4@gmail.com>
2026-03-16 14:59:25 +0400

    added price slider

---
+++ src/widgets/filters/ui/priceSlider.tsx
@@ -0,0 +1,25 @@
+import "./priceSlider.scss";
+import { useState } from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+function PriceSlider() {
+    const [price, setPrice ]=useState([50, 200])
+  return (
+    <><div  className="price-slider">
+      <Slider
+       range 
+      min={0} 
+      max={500} 
+      value={price}
+      onChange={(value)=> setPrice(value as number[]) }
+      marks={{
+        [price[0]] : `${price[0]}$`,
+        [price[1]]: `${price[1]}$`
+      }}
+      />
+  </div>
+    </>
+  );
+}
+export default PriceSlider;
